d7328acca99c24c2a7389457770007809a52a02e
Tautvydas Zukauskas <tautzuk@tutanota.com>
2023-06-06 16:39:32 +0300

    Get full glossary list

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express()
 const port = 3000
+app.use(express.json());
+
+const glossaryDirectory = path.join(__dirname, 'glossary');
 
 app.get('/', (req, res) => {
 	res.send('Linux Glossary')
 })
+
+// get all glossary
+app.get('/api/glossary', (req, res) => {
+	fs.readdir(glossaryDirectory, (err, files) => {
+		if (err) {
+			res.status(500).json({ error: 'Server error' });
+			return;
+		}
+
+		const words = files.map((file) => {
+			const wordData = fs.readFileSync(path.join(glossaryDirectory, file));
+			const word = JSON.parse(wordData);
+			return { ...word }
+		});
+
+		res.json(words);
+	});
+});
 
 app.listen(port, () => {
 	console.log(`App is listening on port ${port}`)
