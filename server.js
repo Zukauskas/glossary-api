4c8b6b93dad7d62941fd38c967b433f4486212d7
Tautvydas Žukauskas <tautzuk@tutanota.com>
2023-08-28 12:34:07 +0300

    Basic solution for pagination

--- server.js
+++ server.js
@@ -1,71 +1,139 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
-const port = 3000
-app.use(express.json())
+const app = express();
+const port = 3000;
+app.use(express.json());
 
-const glossaryDirectory = path.join(__dirname, 'glossary')
+const glossaryDirectory = path.join(__dirname, "glossary");
 
-// function to find available ID
+// Function to find available ID
 const getAvailableID = () => {
-  const files = fs.readdirSync(glossaryDirectory)
-  const onlyIDs = files.map(file => parseInt(path.parse(file).name))
-  return onlyIDs[onlyIDs.length - 1] + 1
-}
+    const files = fs.readdirSync(glossaryDirectory);
+    const onlyIDs = files.map((file) => parseInt(path.parse(file).name));
+    return onlyIDs[onlyIDs.length - 1] + 1;
+};
 
-app.get('/', (req, res) => {
-  res.send('Linux Glossary')
-})
+app.get("/", (req, res) => {
+    res.send("Linux Glossary");
+});
+
 // Add new word
 
-app.post('/api/word', (req, res) => {
-  const { word, definition } = req.body
-  const id = getAvailableID()
-  const createdAt = new Date()
-  const wordFile = path.join(glossaryDirectory, `${id}.json`)
-  const wordData = JSON.stringify({ id, word, definition, createdAt })
+app.post("/api/word", (req, res) => {
+    const { word, definition } = req.body;
+    const id = getAvailableID();
+    const createdAt = new Date();
+    const wordFile = path.join(glossaryDirectory, `${id}.json`);
+    const wordData = JSON.stringify({ id, word, definition, createdAt });
 
-  fs.writeFileSync(wordFile, wordData, (err) => {
-    if (err) {
-      res.status(500).json({ error: 'Server error' })
+    fs.writeFileSync(wordFile, wordData, (err) => {
+        if (err) {
+            res.status(500).json({ error: "Server error" });
+        }
+    });
+
+    res.status(201).json({
+        message: `${word} was added to glossary with id: ${id}`,
+    });
+});
+
+// Get a word by ID
+app.get("/api/glossary/:id", (req, res) => {
+    const wordFile = path.join(glossaryDirectory, `${req.params.id}.json`);
+    if (!fs.existsSync(wordFile)) {
+        res.status(404).json({ error: "Word with such ID does not exist." });
+    } else {
+        const wordData = fs.readFileSync(wordFile);
+        const word = JSON.parse(wordData);
+        res.json(word);
     }
-  })
+});
 
-  res.status(201).json({ message: `${word} was added to glossary with id: ${id}` })
-})
+// Get all glossary
+app.get("/api/glossary", (req, res) => {
+    fs.readdir(glossaryDirectory, (err, files) => {
+        if (err) {
+            res.status(500).json({ error: "Server error" });
+            return;
+        }
 
-// get a word by ID
-app.get('/api/glossary/:id', (req, res) => {
-  const wordFile = path.join(glossaryDirectory, `${req.params.id}.json`)
-  if (!fs.existsSync(wordFile)) {
-    res.status(404).json({ error: 'Word with such ID does not exist.' })
-  } else {
-    const wordData = fs.readFileSync(wordFile)
-    const word = JSON.parse(wordData)
-    res.json(word)
-  }
-})
+        const words = files.map((file) => {
+            const wordData = fs.readFileSync(
+                path.join(glossaryDirectory, file)
+            );
+            const word = JSON.parse(wordData);
+            return { ...word };
+        });
 
-// get all glossary
-app.get('/api/glossary', (req, res) => {
-  fs.readdir(glossaryDirectory, (err, files) => {
-    if (err) {
-      res.status(500).json({ error: 'Server error' })
-      return
+        res.json(words);
+    });
+});
+
+// Get pages
+
+app.get("/api/glossary-page/:page?/:pageSize?", (req, res) => {
+    const { page, pageSize } = req.params;
+    const parsedPage = page ? parseInt(page) : 1;
+    const parsedPageSize = pageSize ? parseInt(pageSize) : 5;
+
+    if (
+        isNaN(parsedPage) ||
+        isNaN(parsedPageSize) ||
+        parsedPageSize < 2 ||
+        parsedPageSize > 20
+    ) {
+        res.status(400).json({ error: "Invalid page or pageSize." });
+        return;
     }
 
-    const words = files.map((file) => {
-      const wordData = fs.readFileSync(path.join(glossaryDirectory, file))
-      const word = JSON.parse(wordData)
-      return { ...word }
-    })
+    fs.readdir(glossaryDirectory, (err, files) => {
+        if (err) {
+            res.status(500).json({ error: "Server error" });
+            return;
+        }
 
-    res.json(words)
-  })
-})
+        const words = files.map((file) => {
+            const wordData = fs.readFileSync(
+                path.join(glossaryDirectory, file)
+            );
+            const word = JSON.parse(wordData);
+            return { ...word };
+        });
+
+        words.sort((a, b) => {
+            const nameComparison = a.word.localeCompare(b.word);
+
+            if (nameComparison !== 0) {
+                return nameComparison;
+            }
+
+            return new Date(a.createdAt) - new Date(b.createdAt);
+        });
+
+        const startIndex = (parsedPage - 1) * parsedPageSize;
+        const endIndex = startIndex + parsedPageSize;
+
+        const paginatedWords = words.slice(startIndex, endIndex);
+
+        const prevPage =
+            parsedPage > 1
+                ? `/api/glossary-page/${parsedPage - 1}/${parsedPageSize}`
+                : "";
+        const nextPage =
+            endIndex < words.length
+                ? `/api/glossary-page/${parsedPage + 1}/${parsedPageSize}`
+                : "";
+
+        res.json({
+            prev: prevPage,
+            list: paginatedWords,
+            next: nextPage,
+        });
+    });
+});
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`)
-})
+    console.log(`App is listening on port ${port}`);
+});
